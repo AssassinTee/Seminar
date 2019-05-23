586a460f2aee12b6703843d04bb404dacf84180a
marvin <mw1996oede@googlemail.com>
2019-05-24 00:07:17 +0200

    Added java embadding - it is currently not working because of bullshit

---
+++ src/java_model_loading/java_model_loading/src/main/java/seminar/java_model_loading/machinelearning/Predictor.java
@@ -0,0 +1,44 @@
+package seminar.java_model_loading.machinelearning;
+
+import java.io.IOException;
+
+//imports
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.io.ClassPathResource;
+
+public class Predictor {
+	MultiLayerNetwork model;
+	public Predictor(String path) throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException
+	{
+		String simpleMlp = new ClassPathResource(path).getFile().getPath();
+		model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
+	}
+	
+	public Predictor(String path1, String path2) throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException
+	{
+		String json = new ClassPathResource(path1).getFile().getPath();
+		String weights = new ClassPathResource(path2).getFile().getPath();
+		model = KerasModelImport.importKerasSequentialModelAndWeights(json, weights);
+	}
+	
+	public INDArray getPrediction(INDArray features)
+	{
+		return model.output(features);
+	}
+	
+	public INDArray getTestInput()
+	{
+		int size = model.getLayer(0).batchSize();
+		INDArray features = Nd4j.zeros(size);
+		for(int i = 0; i < size; ++i)
+		{
+			features.putScalar(new int[] {i}, Math.random() < 0.5? 0 : 1);
+		}
+		return features;
+	}
+}
